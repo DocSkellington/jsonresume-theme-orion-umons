8c251b5aa9c1124c83da13d4d0b8ece3c2352166
radioGiorgio <florent.delgrange@gmail.com>
2019-03-18 12:04:13 +0100

    back to a safe index.js 2019

--- index.js
+++ index.js
@@ -131,15 +131,4 @@ function render(resume) {
 
 module.exports = {
 	render: render
-  mapURL: function(location) {
-      var googleMaps = 'https://www.google.com/maps?q=';
-      var fields = [
-          Handlebars.escapeExpression(location.address || ''),
-          Handlebars.escapeExpression(location.postalCode || ''),
-          Handlebars.escapeExpression(location.city || ''),
-          Handlebars.escapeExpression(location.region || ''),
-          Handlebars.escapeExpression(location.countryCode || '')
-      ];
-      return new Handlebars.SafeString(googleMaps + encodeURIComponent(fields.join(' ').replace(/\s+/g, ' ')));
-  }
 };
