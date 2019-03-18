e175e3fd9ea34f79933f18aa865dfb842076f871
radioGiorgio <florent.delgrange@gmail.com>
2019-03-18 11:42:11 +0100

    styles

--- index.js
+++ index.js
@@ -128,4 +128,15 @@ function render(resume) {
 
 module.exports = {
 	render: render
+  mapURL: function(location) {
+      var googleMaps = 'https://www.google.com/maps?q=';
+      var fields = [
+          Handlebars.escapeExpression(location.address || ''),
+          Handlebars.escapeExpression(location.postalCode || ''),
+          Handlebars.escapeExpression(location.city || ''),
+          Handlebars.escapeExpression(location.region || ''),
+          Handlebars.escapeExpression(location.countryCode || '')
+      ];
+      return new Handlebars.SafeString(googleMaps + encodeURIComponent(fields.join(' ').replace(/\s+/g, ' ')));
+  }
 };
